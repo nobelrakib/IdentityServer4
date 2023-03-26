e42ae360fa70ca850621ab8d764377770b3fe824
nobelrakib03 <nobelrakib>
2023-03-26 17:38:03 +0800

    Added MVC client

---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Movies.Client.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movieList = new List<Movie>();
+            movieList.Add(
+                    new Movie
+                    {
+                        Id = 1,
+                        Genre = "Comics",
+                        Title = "asd",
+                        Rating = "9.2",
+                        ImageUrl = "images/src",
+                        ReleaseDate = DateTime.Now,
+                        Owner = "swn"
+                    }
+                );
+
+            return await Task.FromResult(movieList);
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
